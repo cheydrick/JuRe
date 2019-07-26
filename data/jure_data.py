fc62d419f36f1be48e84eddbaa3c5f2a8c6795e0
Chris Heydrick <cheydrick@gmail.com>
2019-07-26 12:48:42 -0500

    starting on internal data storage structures

--- data/jure_data.py
+++ data/jure_data.py
@@ -1,4 +1,6 @@
 from PIL import Image
+from os import listdir
+from os.path import isfile, join
 
 class JuReImage():
     '''
@@ -6,14 +8,16 @@ class JuReImage():
     for one image, along with other flags, such as whether or not
     the image is flagged for resizing.
     '''
-    def __init__(self, image_file_path):
+    def __init__(self, image_file_path, thumbnail_size):
         self.image_file_path = image_file_path
+        self.thumbnail_size = thumbnail_size
         self.resize_flag = False
         self.image_thumbnail = None
-        pass
+
+        self._create_thumbnail_from_file_path()
 
     def _create_thumbnail_from_file_path(self):
-        self.image_thumbnail = Image.open(self.image_file_path)
+        self.image_thumbnail = Image.open(self.image_file_path).thumbnail(self.thumbnail_size)
         
 
 class JuReData():
@@ -21,6 +25,12 @@ class JuReData():
         # Folders
         self.source_folder = None
         self.destination_folder = None
+
+        # File lists
+        self.image_file_paths_list = []
+
+        # JureImage class instance list
+        self.jure_image_list = []
 
         # Thumbnail options
         self.thumbnail_size = 128, 128
@@ -39,3 +49,44 @@ class JuReData():
         Must be 2-tuple of pixels, such as (128, 128).
         '''
         self.thumbnail_size = size
+
+    def _load_image_file_paths_list(self):
+        for f in listdir(self.source_folder):
+            if isfile(join(self.source_folder, f)):
+                # TODO: Validate that these are image files!!
+                self.image_file_paths_list.append(join(self.source_folder, f))
+
+    def _load_jure_image_list(self):
+        '''
+        This function creates all instances of JuReImage at once before returning.
+        Ideally there needs to be a function that does one-at-a-time, or otherwise
+        yields to the caller, so that I can implement a progress bar in the view.
+        '''
+        for p in self.image_file_paths_list:
+            self.jure_image_list.append(JuReImage(p, self.thumbnail_size))
+
+    def _load_next_jure_image(self):
+        '''
+        Placeholder for an idea.
+        '''
+        pass
+
+if __name__ == '__main__':
+    '''
+    Test code, if this module is run on its own.
+    '''
+    from tkinter import filedialog
+
+    jure_data = JuReData()
+
+    source_folder = filedialog.askdirectory()
+
+    jure_data.set_source_folder(source_folder)
+    jure_data._load_image_file_paths_list()
+    jure_data._load_jure_image_list()
+
+    for p in jure_data.image_file_paths_list:
+        print(p)
+        
+    for i in jure_data.jure_image_list:
+        print(i)
--- jure.py
+++ jure.py
@@ -1,42 +1,7 @@
 from tkinter import Tk
 from PIL import Image
 from views.choose_source_dest import ChooseSourceDestDirView
-
-class JuReImage():
-    '''
-    A JuReImage instance stores the file path and thumbnail
-    for one image, along with other flags, such as whether or not
-    the image is flagged for resizing.
-    '''
-    def __init__(self, image_file_path):
-        self.image_file_path = image_file_path
-        self.resize_flag = False
-        self.image_thumbnail = None
-        pass
-
-class JuReData():
-    def __init__(self):
-        # Folders
-        self.source_folder = None
-        self.destination_folder = None
-
-        # Thumbnail options
-        self.thumbnail_size = 128, 128
-
-    def set_source_folder(self, source_folder):
-        # Add code to validate path
-        self.source_folder = source_folder
-
-    def set_destination_folder(self, destination_folder):
-        # Add code to validate path
-        self.destination_folder = destination_folder
-
-    def set_thumbnail_size(self, size):
-        '''
-        Sets configuration for maximum thumbnail size.
-        Must be 2-tuple of pixels, such as (128, 128).
-        '''
-        self.thumbnail_size = size
+from data.jure_data import JuReData
 
 if __name__ == '__main__':
     jure_data = JuReData()
