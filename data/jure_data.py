deb773ae9a58d93f072990fb3cf4f207c7bb77f0
Chris Heydrick <cheydrick@gmail.com>
2019-07-28 20:07:41 -0500

    JureImage passing test

--- data/jure_data.py
+++ data/jure_data.py
@@ -31,10 +31,9 @@ class JuReImage():
         tmp_image = Image.open(self.source_path + self.file_name)
         new_width = int(tmp_image.width * (self.resize_percentage / 100))
         new_height = int(tmp_image.height * (self.resize_percentage / 100))
-        tmp_image.resize((new_width, new_height))
+        tmp_image = tmp_image.resize((new_width, new_height))
         file_name_no_extension = self.file_name.split('.')[0]
         full_resize_file_path = self.destination_path + file_name_no_extension + '_resized.jpg'
-        #print("Resizing: {} to {} at {} percent ({},{} to {},{}).".format(self.source_path + self.file_name, full_resize_file_path, self.resize_percentage, tmp_image.width, tmp_image.height, new_width, new_height))
         tmp_image.save(full_resize_file_path)
         
 
@@ -43,7 +42,6 @@ class JuReData():
         # Folders
         self.source_path = None
         self.destination_path = None
-
         self.file_names_list = []
 
         # JureImage class instance list
@@ -51,7 +49,6 @@ class JuReData():
 
         # Thumbnail options
         self.thumbnail_size = 128, 128
-
         self.resize_percentage = 25
 
         self.num_file_names = 0
@@ -136,7 +133,11 @@ if __name__ == '__main__':
     '''
     from tkinter import filedialog
 
-    jure_data = JuReData()
+    #jure_data = JuReData()
 
-    source_folder = filedialog.askdirectory()
-    destination_folder = filedialog.askdirectory()
+    source_path = "C:\\Users\\chris\\Code\\JuRe\\Photos2\\" # filedialog.askdirectory()
+    destination_path = "C:\\Users\\chris\\Code\\JuRe\\Resized\\" #filedialog.askdirectory()
+
+    jure_image = JuReImage("TESTPICTURE.jpg", source_path, destination_path, (120,120), resize_percentage = 10)
+    jure_image.resize()
+
