be75b71a618045917d7968cf0e5454ea2bc28229
Chris Heydrick <cheydrick@gmail.com>
2019-07-29 11:08:05 -0500

    main window layout done, nothing connected

--- jure.py
+++ jure.py
@@ -1,5 +1,5 @@
 from tkinter import Tk
-from views.choose_source_dest import ChooseSourceDestDirView
+from views.choose_source_dest import ChooseSourceDestDirView, JuReMainWindow
 from views.progress_bar import LoadSourceImagesProgressView, ResizeImagesProgressView
 from data.jure_data import JuReData
 
@@ -10,24 +10,6 @@ if __name__ == '__main__':
     
     # First present UI for choosing source and destination folder.
     root = Tk()
-    choose_source_dest_view = ChooseSourceDestDirView(root, jure_data.set_source_folder, jure_data.set_destination_folder, resize_all_button_callback = jure_data.set_resize_all_selected, continue_button_callback = jure_data.set_continue_selected)
+    root.title('JuRe - Just Resize!')
+    jure_main_window = JuReMainWindow(root)
     root.mainloop()
-
-    if jure_data.continue_selected:
-        # If "continue" was clicked, set up and run the image processing and progress bar
-        progress_view = LoadSourceImagesProgressView(root, jure_data.num_images)
-        progress_view.on_timer_function = jure_data._load_next_jure_image
-        progress_view.start_timer()
-
-        root.mainloop()
-
-    if jure_data.resize_all_selected:
-        jure_data._load_image_file_paths_list()
-        jure_data._load_jure_image_list()
-        jure_data.set_resize_percentage(choose_source_dest_view.get_resize_percentage())
-        progress_view = ResizeImagesProgressView(root, jure_data.num_images)
-        progress_view.on_timer_function = jure_data._resize_next_jure_image
-        progress_view.start_timer()
-        # If "Resize All" was clicked, do that.
-        root.mainloop()
-        print("Resizing All!")
--- views/choose_source_dest.py
+++ views/choose_source_dest.py
@@ -1,10 +1,7 @@
-from tkinter import Tk, Frame, Button, Label, filedialog, IntVar, StringVar, Entry
+from tkinter import Tk, Frame, Button, Label, filedialog, IntVar, StringVar, Entry, Label
 
 class ChooseSourceDestDirView(Frame):
     '''
-    This displays an interface for choosing the image source folder,
-    and the resized image destination folder.
-
     Args:
         root - Instance of Tk
         set_source_folder_callback - function that takes a folder path as an argument
@@ -61,6 +58,54 @@ class ChooseSourceDestDirView(Frame):
             self.resize_all_button_callback()
 
         self.root.quit()
+
+class JuReMainWindow(Frame):
+    '''
+    Main JuRe window. Source/Destination folder selection, resize setting, and resize button.
+
+    It's up to the caller (ideally some controller class) to set important callbacks.
+    '''
+    def __init__(self, root):
+        self.root = root
+        super().__init__()
+
+        self.set_source_path_callback = None
+        self.set_destination_path_callback = None
+        self.resize_all_callback = None
+
+        self.init_view()
+
+    def init_view(self):
+        self.set_source_path_button = Button(self, text = 'Set Image Source Location', command = self.set_source_path_button_clicked)
+        self.set_destination_path_button = Button(self, text = 'Set Resized Image Location', command = self.set_destination_path_button_clicked)
+        self.resize_percentage_label = Label(self, text = 'Resize Percentage:')
+        self.resize_percent_entry_var = StringVar()
+        self.resize_percentage_entry = Entry(self, textvariable = self.resize_percent_entry_var)
+        self.resize_all_button = Button(self, text = 'Resize All', command = self.resize_all_button_clicked)
+
+        self.set_source_path_button.grid(row = 0, column = 0, columnspan = 2)
+        self.set_destination_path_button.grid(row = 1, column = 0, columnspan = 2)
+        self.resize_percentage_label.grid(row = 2, column = 0)
+        self.resize_percentage_entry.grid(row = 2, column = 1)
+        self.resize_all_button.grid(row = 3, column = 0, columnspan = 2)
+
+        self.pack()
+
+    def set_resize_percentage(self, percentage):
+        self.resize_percent_entry_var.set(str(percentage))
+
+    def get_resize_percentage(self):
+        return int(self.resize_percent_entry_var.get())
+
+    def set_source_path_button_clicked(self):
+        pass
+
+    def set_destination_path_button_clicked(self):
+        pass
+
+    def resize_all_button_clicked(self):
+        pass
+    
 
 if __name__ == '__main__':
     '''
